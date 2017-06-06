fc7f65d9c66c513738447798c4ec113d25bf64a1
Abraão Aires <abraauc@gmail.com>
2017-06-06 15:21:48 -0300

    Testes unitários?
    Criação dos casos apenas.

---
+++ ComunicacaoCriptografada/src/testesUnitarios/TesteCriptografavel.java
@@ -0,0 +1,14 @@
+package testesUnitarios;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteCriptografavel {
+
+	@Test
+	public void testConstrutor() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ ComunicacaoCriptografada/src/testesUnitarios/TesteMensagemDTO.java
@@ -0,0 +1,27 @@
+package testesUnitarios;
+
+import static org.junit.Assert.*;
+
+import java.security.KeyPairGenerator;
+
+import org.junit.Test;
+
+public class TesteMensagemDTO {
+
+	@Test
+	public void test() {
+		KeyPairGenerator gerador ;
+		
+		
+		try{
+		gerador = KeyPairGenerator.getInstance("RSA");
+		gerador.initialize(1024);
+		
+		}catch(Exception e){
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+	}
+
+}
